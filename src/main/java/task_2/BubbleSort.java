9782b84941d6efc2e348cdb3a8256bc22f524028
Svetlana Lvovskaya <svetlana.lvovskaya@gmail.com>
2022-10-29 20:04:47 +0500

    Change task_2, task_3

--- src/main/java/task_2/BubbleSort.java
+++ src/main/java/task_2/BubbleSort.java
@@ -11,31 +11,34 @@ import java.util.logging.*;
 public class BubbleSort {
 
     public static void main(String[] args) throws IOException {
-        Logger logger = Logger.getLogger(BubbleSort.class.getName());
-        FileHandler fh = new FileHandler("log.xml");
-        logger.addHandler(fh);
+        try {
+            Logger logger = Logger.getLogger(BubbleSort.class.getName());
+            FileHandler fh = new FileHandler("log.xml");
+            logger.addHandler(fh);
 
-        XMLFormatter xml = new XMLFormatter();
-        fh.setFormatter(xml);
+            XMLFormatter xml = new XMLFormatter();
+            fh.setFormatter(xml);
 
-        int[] numbers = {4, 2, 1, 5, 44, 14, 666, 0};
+            int[] numbers = {4, 2, 1, 5, 44, 14, 666, 0};
 
-        boolean sorted = false;
+            boolean sorted = false;
 
-        while (!sorted) {
-            sorted = true;
+            while (!sorted) {
+                sorted = true;
 
-            for (int i = 0; i < numbers.length - 1; i++) {
-                if (numbers[i] > numbers[i + 1]) {
-                    int storage = numbers[i];
-                    numbers[i] = numbers[i + 1];
-                    numbers[i + 1] = storage;
-                    sorted = false;
-                    logger.info(Arrays.toString(numbers));
+                for (int i = 0; i < numbers.length - 1; i++) {
+                    if (numbers[i] > numbers[i + 1]) {
+                        int storage = numbers[i];
+                        numbers[i] = numbers[i + 1];
+                        numbers[i + 1] = storage;
+                        sorted = false;
+                        logger.info(Arrays.toString(numbers));
+                    }
                 }
             }
-
+            System.out.println(Arrays.toString(numbers));
+        } catch (IOException e) {
+            throw new IOException("Ошибка!");
         }
-        System.out.println(Arrays.toString(numbers));
     }
 }

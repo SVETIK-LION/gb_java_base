0373e90e78589846aff2177b54c7e41983d6e4cd
Svetlana Lvovskaya <svetlana.lvovskaya@gmail.com>
2022-11-12 20:01:33 +0500

    task_2 second refactoring

--- src/main/java/task_2/QueueLinkedList.java
+++ src/main/java/task_2/QueueLinkedList.java
@@ -1,7 +1,6 @@
 package task_2;
 
 import java.util.LinkedList;
-import java.util.Queue;
 
 
 /* Реализуйте очередь с помощью LinkedList со следующими методами:
@@ -12,32 +11,37 @@ first() - возвращает первый элемент из очереди, 
 
 
 public class QueueLinkedList {
-    public static void main(String[] args){
-        Queue<Integer> Queue_lin_list = new LinkedList<>();
-        // Добавляем элементы:
-        enqueue(Queue_lin_list, 1);
-        enqueue(Queue_lin_list, 2);
-        enqueue(Queue_lin_list, 3);
-        System.out.printf("Очередь после добавления элементов: %s\n", Queue_lin_list);
+    private int size = 0;
+    private final LinkedList<Integer> queue = new LinkedList<Integer>();
 
-        // Возвращаем и удаляем первый элемент из очереди:
-        System.out.printf("Элемент, который был удален: %s\n", dequeue(Queue_lin_list));
-        System.out.printf("Очередь после удаления первого элемента: %s\n", Queue_lin_list);
-
-        // Возращаем первый элемент из очереди не удаляя его:
-        System.out.printf("Первый элемент в очереди: %s\n", first(Queue_lin_list));
-}
-
-    private static void enqueue(Queue<Integer> Queue_lin_list, int elem){
-        Queue_lin_list.add(elem);
-
+    boolean empty() {    //проверяет пустая ли очередь
+        return size == 0;
     }
 
-    private static Integer dequeue(Queue<Integer> Queue_lin_list){
-        return Queue_lin_list.poll();
+    int size() {    // Посмотреть размер очереди
+        System.out.println("Размер очереди: " + size);
+        return size;
     }
 
-    private static Integer first(Queue<Integer> Queue_lin_list){
-        return Queue_lin_list.peek();
+
+    void enqueue(int elem){ // Добавить элемент в очередь
+        queue.add(elem);
+        size++;
+        System.out.println("В очередь добавлен элемент: " + elem);
+    }
+
+    int dequeue(){     //посмотреть и удалить первый элемент очереди
+        if (!empty()){
+            System.out.println("Удален первый элемент: " + queue.poll());
+            return queue.pollLast();
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+    int first(){      // посмотреть первый элемент очереди
+        int elem = queue.peekFirst();
+        size--;
+            System.out.println("Теперь первый элемент в очереди: " + elem);
+        return elem;
     }
 }
